3539519cc72d84e9923a75f3ce3c9fe1c41917cc
justinkim2015 <justinkim2015@gmail.com>
2022-09-24 20:31:49 +0900

    Switch to bulma

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,30 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @post = Post.new
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body)
   end
 end
