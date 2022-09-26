12e3d9b0bd59188b14cdaf8c7faed3d31b71dfc1
justinkim2015 <justinkim2015@gmail.com>
2022-09-26 17:13:11 +0900

    Move logic to model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
+    @except_current_user = User.not_self(current_user.id)
     @friend_request = FriendRequest.new
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,8 @@ class User < ApplicationRecord
 
   has_many :recieved_friend_requests, foreign_key: "reciever_id", class_name: "FriendRequest"
   has_many :sent_friend_requests, foreign_key: "requester_id", class_name: "FriendRequest"
+
+  def self.not_self(id)
+    User.where.not(id: id)
+  end
 end
