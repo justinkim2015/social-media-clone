3c2da82bc9527b5d2a6032eb9f1770479b4c70a3
justinkim2015 <justinkim2015@gmail.com>
2022-09-27 13:26:01 +0900

    Write unit tests to test associations

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,18 +1,25 @@
 require "test_helper"
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-
   test 'should not save post without body' do
     post = Post.new
     assert_not post.save, 'Saved the post without a body'
   end
 
-  test 'should add user_id to db'
+  test 'should add user_id to db' do
     user = User.first
-    post = user.posts.build(body: "hello")
+    post = user.posts.build(body: "hello"), 'post is unable to be saved'
 
-    assert post 
+    assert post
+  end
+
+  test 'should be able to access posts' do
+    user = User.first
+    assert user.posts, 'post unable to be reached'
+  end
+
+  test 'should be able to access friends' do
+    user = User.first
+    assert user.friends, 'friends unable to accessed'
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should be able to see sent friend requests" do
+    user = User.first
+    assert user.sent_friend_requests
+  end
+
+  test "should be able to see recieved friend requests" do
+    user = User.first
+    assert user.recieved_friend_requests
+  end
 end
