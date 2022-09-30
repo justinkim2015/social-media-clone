7eb43a5aef96db4c6c729189b5b985fa2e614db6
justinkim2015 <justinkim2015@gmail.com>
2022-09-30 11:05:27 +0900

    Create likes scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_30_004133) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_30_015150) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_30_004133) do
     t.bigint "friend_b_id", null: false
     t.index ["friend_a_id"], name: "index_friendships_on_friend_a_id"
     t.index ["friend_b_id"], name: "index_friendships_on_friend_b_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "posts", force: :cascade do |t|
