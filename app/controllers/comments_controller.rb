ba8ee6d769e67c1246601fcb3a1fe3781637f55e
justinkim2015 <justinkim2015@gmail.com>
2022-09-30 21:47:15 +0900

    Create comment function

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,22 @@
 class CommentsController < ApplicationController
+  def new
+    @comment = Comment.new
+    @post = Post.find(params[:post_id])
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+
+    if @comment.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body, :user_id, :post_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  resources :posts, :friend_requests, :friendships, :likes
+  resources :posts, :friend_requests, :friendships, :likes, :comments
   resources :users, only: [:index, :show]
 end
