42447552bcb29b16b34d04ca567dda8c6687d9fb
justinkim2015 <justinkim2015@gmail.com>
2022-10-18 11:36:01 +0900

    Shorten code in full_post with partials

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @post = Post.new
+    @comment = current_user.comments.build
   end
 
   def update_profile
